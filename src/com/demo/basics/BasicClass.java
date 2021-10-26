c12dcb0ca8b9b239afa2a688f8306841fa16ceb9
pepper45 <zapper.ethan@gmail.com>
2021-10-27 04:56:15 +0530

    basic class

---
+++ src/com/demo/basics/BasicClass.java
@@ -0,0 +1,25 @@
+package com.demo.basics;
+
+//Default package, always imported
+import java.lang.*;
+
+//Public access modifier, available everywhere
+public class BasicClass {
+
+    //Static keyword, belongs to the class
+    static Object obj = new Object();
+
+    //Object reference, references are memory address
+    static Class classObj;
+
+    static Integer intObj = new Integer(6);
+
+    //User defined class
+    //static Dog dog = new Dog();
+
+    public static void main(String[] args) {
+        System.out.println(obj);
+        System.out.println(classObj);
+        System.out.println(intObj);
+    }
+}
